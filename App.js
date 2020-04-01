82e4087da3298166a9dba817b8b5acb64b46394c
berbiche <berbiche@gmail.com>
2020-04-01 13:46:28 +0200

    début navigation

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'reat-navigation-stack';
+
 import AllMessagesScreen from './src/screen/AllMessagesScreen';
 
-export default function App() {
-    return <AllMessagesScreen />;
-}
+const navigator = createStackNavigator(
+    {
+        Messages: AllMessagesScreen
+    },
+    {
+        initialRouteName: 'Messages',
+        defaultNavigationOptions: {
+            title: 'Testeur de présence'
+        }
+    }
+);
+
+export default createAppContainer(navigator);
