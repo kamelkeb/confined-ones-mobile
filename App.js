d9d329426cd702bae15bc7c8f26f1e29766e9074
berbiche <berbiche@gmail.com>
2020-04-01 12:13:10 +0200

    factorisation en vue de mise en place de la navigation

--- App.js
+++ App.js
@@ -1,75 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+import AllMessagesScreen from './src/screen/AllMessagesScreen';
 
 export default function App() {
-    const [ listeVisible, setListeVisible ] = useState(false);
-    const data = [
-        {
-            id: 1,
-            name: 'Martin',
-            age: 42
-        },
-        {
-            id: 2,
-            name: 'Elisa',
-            age: 22
-        },
-        {
-            id: 3,
-            name: 'Paul',
-            age: 88
-        },
-        {
-            id: 4,
-            name: 'Lee',
-            age: 33
-        },
-        {
-            id: 5,
-            name: 'Mehdi',
-            age: 19
-        },
-        {
-            id: 6,
-            name: 'Marc',
-            age: 42
-        },
-        {
-            id: 7,
-            name: 'Martin',
-            age: 46
-        }
-    ];
-
-    return (
-        <View style={{ ...styles.container, marginTop: 60 }}>
-            <Text style={styles.text}>Salut Ifocop!</Text>
-            <Button title="Afficher ou cacher la liste des messages" onPress={() => setListeVisible(!listeVisible)} />
-            {listeVisible ? (
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <View
-                            style={{ ...styles.container, borderColor: 'black', borderWidth: 1, margin: 5, padding: 5 }}
-                        >
-                            <Text>{`Bonjour je m'appelle ${item.name} et j'ai ${item.age} ans.`}</Text>
-                        </View>
-                    )}
-                    keyExtractor={(item) => item.id.toString()}
-                />
-            ) : null}
-        </View>
-    );
+    return <AllMessagesScreen />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    textStyle: {
-        fontSize: 32
-    }
-});
---
+++ src/screen/AllMessagesScreen.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+
+const AllMessagesScreen = (props) => {
+    const [ listeVisible, setListeVisible ] = useState(false);
+    const data = [
+        {
+            id: 1,
+            name: 'Martin',
+            age: 42
+        },
+        {
+            id: 2,
+            name: 'Elisa',
+            age: 22
+        },
+        {
+            id: 3,
+            name: 'Paul',
+            age: 88
+        },
+        {
+            id: 4,
+            name: 'Lee',
+            age: 33
+        },
+        {
+            id: 5,
+            name: 'Mehdi',
+            age: 19
+        },
+        {
+            id: 6,
+            name: 'Marc',
+            age: 42
+        },
+        {
+            id: 7,
+            name: 'Martin',
+            age: 46
+        }
+    ];
+    return (
+        <SafeAreaView style={{ ...styles.container, marginTop: 60 }}>
+            <Text style={styles.text}>Salut Ifocop!</Text>
+            <Button title="Afficher ou cacher la liste des messages" onPress={() => setListeVisible(!listeVisible)} />
+            {listeVisible ? (
+                <FlatList
+                    data={data}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity onPress={() => alert('Joli click!')}>
+                            <View
+                                style={{
+                                    ...styles.container,
+                                    borderColor: 'black',
+                                    borderWidth: 1,
+                                    margin: 5,
+                                    padding: 5
+                                }}
+                            >
+                                <Text>{`Bonjour je m'appelle ${item.name} et j'ai ${item.age} ans.`}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            ) : null}
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textStyle: {
+        fontSize: 32
+    }
+});
+
+export default AllMessagesScreen;
