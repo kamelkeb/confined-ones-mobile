6d1d07311c60f60e1810e78cdbe4bae23d6d4fad
berbiche <berbiche@gmail.com>
2020-04-01 11:05:32 +0200

    Usage de button

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 
 export default function App() {
+    const [ listeVisible, setListeVisible ] = useState(false);
     const data = [
         {
             id: 1,
@@ -39,18 +40,24 @@ export default function App() {
             age: 46
         }
     ];
+
     return (
         <View style={{ ...styles.container, marginTop: 60 }}>
             <Text style={styles.text}>Salut Ifocop!</Text>
-            <FlatList
-                data={data}
-                renderItem={({ item }) => (
-                    <View style={{ ...styles.container, borderColor: 'black', borderWidth: 1, margin: 5, padding: 5 }}>
-                        <Text>{`Bonjour je m'appelle ${item.name} et j'ai ${item.age} ans.`}</Text>
-                    </View>
-                )}
-                keyExtractor={(item) => item.id.toString()}
-            />
+            <Button title="Afficher ou cacher la liste des messages" onPress={() => setListeVisible(!listeVisible)} />
+            {listeVisible ? (
+                <FlatList
+                    data={data}
+                    renderItem={({ item }) => (
+                        <View
+                            style={{ ...styles.container, borderColor: 'black', borderWidth: 1, margin: 5, padding: 5 }}
+                        >
+                            <Text>{`Bonjour je m'appelle ${item.name} et j'ai ${item.age} ans.`}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            ) : null}
         </View>
     );
 }
