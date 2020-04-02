eb177ffa9f8c25b5fe4c3b75ca2c87b812f5fd6d
berbiche <berbiche@gmail.com>
2020-04-02 11:08:40 +0200

    notre première navigation entre screens

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+// NavigationContainer est le composant qui va gérer l'arbre de navigation (le scénario de navigation),
+// cet objet contiendra l'état de la navigation.
+// Il doit être u wrapper englobant toute la structure des navigateurs.
+import { NavigationContainer } from '@react-navigation/native';
+// createStackNavigator est une fonction qui retourne un objet ayant deux propriétés,
+// chacune est un composant react servant à configurer le navigateur: Screen et Navigator.
+// Navigator contiendra des composant Screen comme enfants pour définir ses routes.
+import { createStackNavigator } from '@react-navigation/stack';
 
 import AllMessagesScreen from './src/screen/AllMessagesScreen';
 import WelcomeScreen from './src/screen/WelcomeScreen';
+import LoginScreen from './src/screen/LoginScreen';
+
+const { Navigator, Screen } = createStackNavigator();
 
 const App = (props) => {
-    return <AllMessagesScreen />;
+    return (
+        <NavigationContainer>
+            {/* La configuration des routes se spécifie à travers des props
+            du composant Navigator */}
+            <Navigator initialRouteName="Welcome">
+                <Screen name="Welcome" component={WelcomeScreen} />
+                <Screen name="AllMessages" component={AllMessagesScreen} options={{ title: 'Messages' }} />
+                <Screen name="Login" component={LoginScreen} />
+            </Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default App;
--- src/screen/WelcomeScreen.js
+++ src/screen/WelcomeScreen.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity, SafeAreaView } from 'react-native';
 
-const WelcomeScreen = (props) => {
-    return <Text style={styles.text}>Bienvenue chère utilisiatrice!</Text>;
+// Chaque screen (mis sous Navigation container) recevram parmi les props
+// un objet nommé navigation
+const WelcomeScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView>
+            <Text style={styles.text}>Bienvenue chère utilisiatrice!</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('AllMessages')}>
+                <Text style={styles.clickable}>Voir tous les messages</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-    text: { fontSize: 38 }
+    text: { fontSize: 38 },
+    clickable: {
+        borderColor: 'black',
+        borderWidth: 1,
+        margin: 5,
+        padding: 5
+    }
 });
+
+export default WelcomeScreen;
